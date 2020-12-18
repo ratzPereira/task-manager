b8456d1ae6ef63c926be52c8f1bd60db4e8777a5
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-12-18 12:49:26 -0100

    Created endpoints to find user by id, find all users, find task by id and find all tasks

--- src/index.js
+++ src/index.js
@@ -14,9 +14,32 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {       //will fetch all
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
@@ -25,12 +48,33 @@ app.post('/task', (req,res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
 
+
+app.get('/task', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+
+app.get('/task/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404)
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port)
