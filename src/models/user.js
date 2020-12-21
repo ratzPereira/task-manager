9339a4fb582a08da544d0e85d1f412a43d973fba
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-12-21 14:29:56 -0100

    login implemented , unique email added in the model

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase:true,
@@ -41,6 +42,23 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
 
 userSchema.pre('save', async function (next) {   //we run this before the user is saved
     const user = this
