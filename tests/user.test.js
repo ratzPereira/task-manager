db80122142a5acb748a382dcc9b9ed46b76e425e
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-03 21:10:26 -0100

    added more tests
    now we can test if the user is logged in and if can delete his account or read profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,20 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { send } = require('@sendgrid/mail')
 
 
-
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: "Test User",
     email: "TestUser@email.com",
-    password: "123fortester"
+    password: "123fortester",
+    tokens: [{
+        token : jwt.sign({ _id: userOneId}, process.env.JWT_SECRET)
+    }]
 }
 
 
@@ -41,4 +48,38 @@ test('Should not to be able to login', async () => {
         email: userOne.email,
         password: "password"
     }).expect(400)
+})
+
+
+test('Should read the user profile', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+
+test('Should fail when trying to read a profile not logged in', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+
+test('Delete user account', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+
+test('Cant delete account unauthorized', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
 })
